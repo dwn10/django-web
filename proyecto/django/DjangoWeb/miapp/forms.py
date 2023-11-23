6293aa199605bea76c50caf8948e39f0fc939bdb
Darwin Paz <142513909+dwn10@users.noreply.github.com>
2023-11-23 22:02:43 +0100

    23-11-2023

--- proyecto/django/DjangoWeb/miapp/forms.py
+++ proyecto/django/DjangoWeb/miapp/forms.py
@@ -1,18 +1,23 @@
 from django import forms
+from django.core import validators
 
 class FormArticle(forms.Form):
 
     title = forms.CharField(
         label = "Titulo",
         max_length=40,
-        required=False,
+        required=True,
         widget=forms.TextInput(
             attrs={
                 'placeholder': ' Introduce el titulo',
                 'class': 'titulo_form_article'
             }
         )
-    )
+    ),
+    validators=[
+        validators.MinLengthValidator(4, 'Título demasiado corto'),
+        validators.RegexValidator('^[A-Za-z0-9]*$', 'Título mal formado', 'invalid_title')
+    ]
 
     content = forms.CharField(
         label = "Contenido",
