e53e31db826a3bb886e8c5d84f973aed71511bb5
Darwin Paz <142513909+dwn10@users.noreply.github.com>
2024-05-02 00:28:48 +0200

    char

---
+++ PYTHON/streamlit/app.py
@@ -0,0 +1,82 @@
+# 1) https://pypi.org/project/streamlit/      pip install streamlit
+
+# 2) pip3 install -r requirements.txt
+
+# 3) Terminal: cd PYTHON/streamlit>streamlit run app.py
+# 4) Welcome to Streamlit! / You can now view your Streamlit app in your browser.
+        # Local URL: http://localhost:8501
+        # Network URL: http://100.126.10.200:8501
+
+        # Survey_Results.xlsx
+
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Resultados de la encuesta')
+st.header('Resultados de la encuesta 2024')
+st.subheader('¿Fue útil el tutorial?')
+
+### --- Cargar DataFrame
+excel_file = 'encuesta.xlsx'
+sheet_name = 'DATA'
+
+df = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='B:D',
+                   header=3)
+
+df_participants = pd.read_excel(excel_file,
+                                sheet_name= sheet_name,
+                                usecols='F:G',
+                                header=3)
+df_participants.dropna(inplace=True)
+
+# --- STREAMLIT SELECTION
+department = df['Department'].unique().tolist()
+ages = df['Age'].unique().tolist()
+
+age_selection = st.slider('Age:',
+                        min_value= min(ages),
+                        max_value= max(ages),
+                        value=(min(ages),max(ages)))
+
+department_selection = st.multiselect('Department:',
+                                    department,
+                                    default=department)
+
+# --- Filtro de datos de datos basado en la selección
+mask = (df['Age'].between(*age_selection)) & (df['Department'].isin(department_selection))
+number_of_result = df[mask].shape[0]
+st.markdown(f'*Available Results: {number_of_result}*')
+
+# --- Grupo de datos de datos después de la selección
+df_grouped = df[mask].groupby(by=['Rating']).count()[['Age']]
+df_grouped = df_grouped.rename(columns={'Age': 'Votes'})
+df_grouped = df_grouped.reset_index()
+
+# --- Gráfico de barras
+bar_chart = px.bar(df_grouped,
+                   x='Rating',
+                   y='Votes',
+                   text='Votes',
+                   color_discrete_sequence = ['#F63366']*len(df_grouped),
+                   template= 'plotly_white')
+st.plotly_chart(bar_chart)
+
+# --- Mostrar imagen y marco de datos
+col1, col2 = st.columns(2)
+image = Image.open('images/survey.jpg')
+col1.image(image,
+        caption='Designed by slidesgo / Freepik',
+        use_column_width=True)
+col2.dataframe(df[mask])
+
+# --- Gráfico Char
+pie_chart = px.pie(df_participants,
+                title='Total No. of Participants',
+                values='Participants',
+                names='Departments')
+
+st.plotly_chart(pie_chart)
