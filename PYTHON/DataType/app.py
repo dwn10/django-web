e17dd527ccc0b2bdced33bf1550c22a44d289412
Darwin Paz <142513909+dwn10@users.noreply.github.com>
2024-06-28 01:09:55 +0200

    qr_generator

--- PYTHON/DataType/app.py
+++ PYTHON/DataType/app.py
@@ -12,6 +12,9 @@ import ast      # Import ast for safer evaluation
 import random   # Password generator
 import string   # Password generator
 import pyshorteners # URL Shortener https://pypi.org/project/pyshorteners/   pip install pyshorteners
+import qrcode       # https://pypi.org/project/qrcode/       pip install qrcode
+from io import BytesIO
+
 
 #--------------------------------
 # Braille translation dictionary
@@ -43,12 +46,12 @@ braille_dict = {
 #Page config
 #--------------------------------
 st.set_page_config(page_title="PAZ Tools", page_icon="images/EC-IT-LOGO-1.png", layout="centered")
-st.image("images/binary-code-tunel.jpg")
+#st.image("images/binary-code-tunel.jpg")
 
 #--------------------------------
 # Option menu
 #--------------------------------
-selected_option = st.sidebar.selectbox(
+selected_option = st.sidebar.radio(
     "Elige una herramienta:",
     ["Calculadora Básica",
         "Identificador de Tipos Datos",
@@ -56,7 +59,10 @@ selected_option = st.sidebar.selectbox(
         "Conversor de Texto a Hexadecimal",
         "Traductor a Braille",
         "Generador de Password",
-        "Acortador de URL"]
+        "Acortador de URL",
+        "IMC - Calc. de Índice de Masa Corporal",
+        "Calculador de Tiempo",
+        "Generador de QR"]
 )
 
 #--------------------------------
@@ -112,6 +118,7 @@ if selected_option == "Calculadora Básica":
 #--------------------------------
 elif selected_option == "Identificador de Tipos Datos":
     st.subheader("Identificador de Tipos Datos")
+    st.image("images/ec-it-tools-3.JPG", use_column_width=True)
     data_str = st.text_input("Ingresa un valor:")  # Get input as a string
 
     if data_str:
@@ -155,6 +162,7 @@ elif selected_option == "Identificador de Tipos Datos":
 #--------------------------------
 elif selected_option == "Conversor a Binario":
     st.subheader("Conversor a Binario")
+    st.image("images/ec-it-tools-2.JPG", use_column_width=True)
     text = st.text_input("Ingresa un texto:")
     if text:
         binary_str = "".join(format(ord(char), '08b') for char in text)
@@ -165,7 +173,8 @@ elif selected_option == "Conversor a Binario":
 # Binary hexadecimal
 #--------------------------------
 elif selected_option == "Conversor de Texto a Hexadecimal":
-    st.header("Conversor de Texto a Hexadecimal")
+    st.subheader("Conversor de Texto a Hexadecimal")
+    st.image("images/ec-it-tools-3.JPG", use_column_width=True)
     input_text = st.text_input("Introduce texto:")
     if input_text:
         hex_output = "".join(format(ord(c), "02x") for c in input_text)
@@ -177,6 +186,7 @@ elif selected_option == "Conversor de Texto a Hexadecimal":
 #--------------------------------
 elif selected_option == "Traductor a Braille":
     st.subheader("Traductor a Braille")
+    st.image("images/ec-it-tools-2.JPG", use_column_width=True)
     text = st.text_input("Ingresa un texto (solo letras minúsculas):").lower()
     if text:
         braille_text = "".join(braille_dict.get(char, '') for char in text)
@@ -184,10 +194,11 @@ elif selected_option == "Traductor a Braille":
         st.code(braille_text)
 
 #--------------------------------
-# Password generator section
+# Password generator
 #--------------------------------
 elif selected_option == "Generador de Password":
     st.subheader("Generador de Password")
+    st.image("images/ec-it-tools-3.JPG", use_column_width=True)
 
     def generate_password(length=12):
         """Generates a strong random password."""
@@ -208,6 +219,7 @@ elif selected_option == "Generador de Password":
 #--------------------------------
 elif selected_option == "Acortador de URL":
     st.subheader("Acortador de URL")
+    st.image("images/ec-it-tools-2.JPG", use_column_width=True)
 
     url_to_shorten = st.text_input("Ingrese la URL:")
 
@@ -220,3 +232,122 @@ elif selected_option == "Acortador de URL":
             st.code(shortened_url)
         except Exception as e:
             st.error(f"Error al acortar la URL: {e}")
+
+#--------------------------------
+# BMI Calculator
+#--------------------------------
+elif selected_option == "IMC - Calc. de Índice de Masa Corporal":
+    st.subheader("Calculadora de Índice de Masa Corporal (IMC)")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        weight = st.number_input("Peso 80(kg):", value=0)
+    with col2:
+        height = st.number_input("Altura 1,78(m):", value=0.0)
+
+    if st.button("Calcular IMC"):
+        if weight > 0 and height > 0:
+            bmi = weight / (height ** 2)
+            st.write(f"Tu IMC es: {bmi:.2f}")
+
+            if bmi < 18.5:
+                st.info("Bajo peso")
+            elif 18.5 <= bmi < 25:
+                st.success("Peso normal")
+            elif 25 <= bmi < 30:
+                st.warning("Sobrepeso")
+            else:
+                st.error("Obesidad")
+        else:
+            st.error("Por favor, ingrese valores válidos para peso y altura.")
+
+#--------------------------------
+# Time Calculator
+#--------------------------------
+elif selected_option == "Calculador de Tiempo":
+    st.subheader("Calcula la diferencia horaria entre una fecha determinada y la fecha actual.")
+    # Input date in a 50% width container
+    input_container = st.container()
+    with input_container:
+        input_date_str = st.date_input("Introduce una fecha (AAAA-MM-DD):")
+        input_date = input_date_str
+        
+    # Calculate button centered below the date input
+    calculate_button = st.button("Calcular")
+
+    # Display results only when button is clicked
+    if calculate_button and input_date_str:
+        def calculate_time_difference(input_date):
+            """Calculate time difference between input_date and today."""
+            today = date.today()
+
+            # Calculate exact difference using datetime objects
+            start_datetime = datetime.combine(input_date, time.min)
+            end_datetime = datetime.combine(today, time.min)
+            time_difference = end_datetime - start_datetime
+
+            years_diff = time_difference.days // 365
+            remaining_days = time_difference.days % 365
+
+            months_diff = remaining_days // 30
+            days_diff = remaining_days % 30
+
+            total_hours = time_difference.total_seconds() / 3600
+
+            # Day of the week and leap year
+            day_names_es = {
+                "Monday": "Lunes",
+                "Tuesday": "Martes",
+                "Wednesday": "Miércoles",
+                "Thursday": "Jueves",
+                "Friday": "Viernes",
+                "Saturday": "Sábado",
+                "Sunday": "Domingo",
+            }
+            day_of_week_es = day_names_es[input_date.strftime("%A")]
+            is_leap_year = input_date.year % 4 == 0 and (input_date.year % 100 != 0 or input_date.year % 400 == 0)
+
+            return years_diff, months_diff, days_diff, total_hours, day_of_week_es, is_leap_year
+
+        years, months, days, hours, day_name, leap_year = calculate_time_difference(input_date)
+
+        st.subheader(f"Diferencia de tiempo entre {input_date_str} & {date.today()}:")
+        st.write(f"Años: {years}")
+        st.write(f"Mese(s): {months}")
+        st.write(f"Día(s): {days}")
+        st.write(f"Total Horas: {hours:.2f}")  # Rounded to 2 decimal places
+
+        st.write(f"Día de la semana: {day_name}")
+        st.write(f"Fue un año bisiesto: {'Sí' if leap_year else 'No'}")
+    else:
+        # Display a message if the button hasn't been clicked yet
+        st.info("Haz clic en 'Calcular' para obtener la diferencia de tiempo.")
+        
+#--------------------------------
+# QR code generator
+#--------------------------------
+elif selected_option == "Generador de QR":
+    st.subheader("Generador de Código QR")
+    qr_data = st.text_input("Ingrese el texto o URL para el código QR:")
+
+    if st.button("Generar QR"):
+        if qr_data:
+            qr = qrcode.QRCode(version=1, box_size=10, border=5)
+            qr.add_data(qr_data)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+
+            # Display QR code
+            st.image(img)
+
+            # Download button
+            buffered = BytesIO()
+            img.save(buffered, format="PNG")
+            btn = st.download_button(
+                label="Descargar QR",
+                data=buffered.getvalue(),
+                file_name="qrcode.png",
+                mime="image/png"
+            )
+        else:
+            st.warning("Por favor, ingrese un texto o URL.")
